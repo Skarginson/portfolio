cf3dc1586ce35d3e773bf4168b3a99d6942032ec
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-12-09 16:38:30 +0100

    added bosco by agham

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,23 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
   const currentLocation = window.location.pathname;
-  const navLinks = document.querySelectorAll("nav ul li a");
+  const navLinks = document.querySelectorAll('nav ul li a');
 
   navLinks.forEach((link) => {
     if (link.href.includes(currentLocation)) {
-      link.classList.add("active");
+      link.classList.add('active');
     }
   });
+});
+
+const container = document.getElementById('hover');
+const image = document.getElementById('hover-image');
+
+container.addEventListener('mouseenter', () => {
+  console.log('hovered');
+  image.style.display = 'block';
+});
+
+container.addEventListener('mouseleave', () => {
+  console.log('left');
+  image.style.display = 'none';
 });
